e65cf879b763c73166d8980d12cf0e911a9bcc8e
shammy0923 <roseshammy@gmail.com>
2022-02-28 22:07:03 -0500

    Forcing player to choose rock, paper, or scissors

--- scripts.js
+++ scripts.js
@@ -4,6 +4,12 @@ function computerPlay() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function Play() {
+let playerSelection = prompt("Choose Rock, paper, or scissors!").toLowerCase();
+
+while (!choices.includes(playerSelection)) {
+    playerSelection = prompt("You must choose ROCK, PAPER, or SCISSORS.").toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
     
 }
