fc3404c39fb53718aefad107dd31c25eec204e1c
shammy0923 <roseshammy@gmail.com>
2022-02-28 22:26:24 -0500

    Added Round Logic

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ function computerPlay() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
+const computerSelection = computerPlay();
 let playerSelection = prompt("Choose Rock, paper, or scissors!").toLowerCase();
 
 while (!choices.includes(playerSelection)) {
@@ -11,5 +12,29 @@ while (!choices.includes(playerSelection)) {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection == computerSelection) {
+        return "We have a tie!";
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            return "You lose!";
+        } else if (computerSelection == "scissors"){
+            return "You win!";
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "scissors") {
+            return "You lose!";
+        } else if (computerSelection == "rock") {
+            return "You win!";
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            return "You lose!";
+        } else if (computerSelection == "paper") {
+            return "You win!";
+        }
+    }
 }
+
+const Result = playRound(playerSelection, computerSelection);
+
+alert(`You chose ${playerSelection} and your opponent chose ${computerSelection}. ` + Result);
