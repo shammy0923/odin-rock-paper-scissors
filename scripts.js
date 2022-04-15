ecf970c4856e601cbaa12e81633f8967c32b6c93
shammy0923 <roseshammy@gmail.com>
2022-04-14 21:30:39 -0400

    Finalized score, divs, and results.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,50 @@
 const choices = ["rock", "paper", "scissors"];
 
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+const playerScore = document.getElementById("player-score");
+const botScore = document.getElementById("bot-score");
+
+const textResult = document.getElementsByClassName("text-result")
+
+const Buttons = [rockButton, paperButton, scissorsButton]
+
 function computerPlay() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
-// const computerSelection = computerPlay();
-// let playerSelection = prompt("Choose Rock, paper, or scissors!").toLowerCase();
+for (let i in Buttons) {
+    Buttons[i].addEventListener("click", function() {
+        let computerSelection = computerPlay();
 
-// while (!choices.includes(playerSelection)) {
-//     playerSelection = prompt("You must choose ROCK, PAPER, or SCISSORS.").toLowerCase();
-// }
+        playerSelection = Buttons[i].id;
+
+        let Result = playRound(playerSelection, computerSelection);
+
+        if (Result == "You win!") {
+            playerScore.innerHTML = parseInt(playerScore.innerHTML) + 1;
+        } else if (Result == "You lose!") {
+            botScore.innerHTML = parseInt(botScore.innerHTML) + 1;
+        } else if (Result == "We have a tie!") {
+            textResult.innerText = "Yall tied!";
+        }
+
+        if (parseInt(playerScore.innerHTML) >= 5 || parseInt(botScore.innerHTML) >= 5) {
+
+            if (parseInt(playerScore.innerHTML) >= 5) {
+                textResult.innerHTML = "Player won!";
+            } else if (parseInt(botScore.innerHTML) >= 5) {
+                textResult.innerHTML = "Player lost!";
+            }
+
+            playerScore.innerHTML = 0;
+            botScore.innerHTML = 0;
+            textResult.innerText = "";
+        }
+    });
+}
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
@@ -36,5 +71,3 @@ function playRound(playerSelection, computerSelection) {
 }
 
 const Result = playRound(playerSelection, computerSelection);
-
-alert(`You chose ${playerSelection} and your opponent chose ${computerSelection}. ` + Result);
